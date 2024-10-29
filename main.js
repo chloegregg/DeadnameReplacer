4daddc479d82e535ce5af8239d1552f084479a86
chloegregg <charliejamesgregg@gmail.com>
2024-10-29 19:15:19 +1100

    brokens

--- main.js
+++ main.js
@@ -20,10 +20,39 @@ function fixElement(element) {
         child = child.nextSibling
     }
 }
-document.addEventListener("click", event => {
-    console.log(event)
-}, true)
+console.log("start")
+// setTimeout(()=>fixElement(document.body), 1000)
+const id = setInterval(() => {
+    fixElement(document.body)
+    console.log("fixx")
+}, 10)
+// window.addEventListener('load', function () {
+//     fixElement(document.body)
+//     clearInterval(id)
+// })
+// document.addEventListener("click", event => {
+//     console.log(event.target)
+//     fixElement(event.target)
+// }, true)
 
+// // https://stackoverflow.com/a/5379408
+// function getSelectionText() {
+//     let text = "";
+//     const activeEl = document.activeElement;
+//     const activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+
+//     if (
+//         (activeElTagName == "textarea") || (activeElTagName == "input" &&
+//             /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
+//         (typeof activeEl.selectionStart == "number")
+//     ) {
+//         text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+//     } else if (window.getSelection) {
+//         text = window.getSelection().toString();
+//     }
+
+//     return text;
+// }
 
 // const elements = document.getElementsByName("*")
 // elements.forEach(element => {
